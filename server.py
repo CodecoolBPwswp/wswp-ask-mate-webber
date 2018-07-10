1179a6e73809be31887cab43d29e969aeae20be7
Gábor Kovács <gabor950601@gmail.com>
2018-07-10 10:33:30 +0200

    First commit/server init

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/list')
+def index():
+    return
+
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True,
+    )
