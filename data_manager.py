a092062ccf9bc03780d577aab4734168d7637f3c
Gábor Kovács <gabor950601@gmail.com>
2018-07-10 15:05:54 +0200

    get question_by_id

--- data_manager.py
+++ data_manager.py
@@ -3,6 +3,8 @@ import connection
 question_table = connection.get_data('sample_data/question.csv') #list of dict
 answer_table = connection.get_data('sample_data/answer.csv') #list of dict
 
+QUESTION_HEADERS = ["id", "submission_time", "view_number", "vote_number", "title", "message,image"]
+ANSWER_HEADERS = ["id", "submission_time", "vote_number", "question_id", "message","image"]
 
 def get_next_answer_id():
     last_id = 0
@@ -13,6 +15,10 @@ def get_next_answer_id():
     return next_id
 
 
-def get_questions():
-    return connection.get_data("sample_data/question.csv")
+def get_question_byid(q_id):
+    question = [question for question in question_table if int(question['id']) == q_id][0]
+    answers = [answer for answer in answer_table if int(answer['question_id']) == q_id]
+    return question, answers
+
+
 
