54a835416a53fc4ade2f3999b4b8b89592df36a3
Gábor Kovács <gabor950601@gmail.com>
2018-07-24 10:31:20 +0200

    Some render template changed to redirect, and delete images from folder implemented

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 import data_manager
 import operator
+import os
 from operator import itemgetter
 import time
 
@@ -37,17 +38,19 @@ def add_question():
 
 @app.route("/question/<question_id>/delete")
 def delete_question(question_id):
+    data = request.args.to_dict()
+    os.remove(os.path.join(app.config['UPLOADED_PHOTOS_DEST'], data['image']))
     data_manager.delete(question_id)
     return redirect('/list')
 
 
-@app.route("/answer/<answer_id>/delete", methods=['POST'])
+@app.route("/answer/<answer_id>/delete")
 def delete_answer(answer_id):
-    html_file = "question_with_answers.html"
     question_id = data_manager.get_question_id_by_answer_id(answer_id)
+    data = request.args.to_dict()
+    os.remove(os.path.join(app.config['UPLOADED_PHOTOS_DEST'], data['image']))
     data_manager.delete_answer(answer_id)
-    question, answers = data_manager.get_question_byid(int(question_id))
-    return render_template(html_file, question=question, answers=answers, q_head=q_head, a_head=a_head)
+    return redirect('question/{}'.format(question_id))
 
 
 @app.route("/question/<int:question_id>", methods=['GET', 'POST'])
@@ -59,12 +62,12 @@ def question(question_id):
             filename = photos.save(request.files['photo'])
             data = request.form.to_dict()
             data["image"] = filename
-            question, answers = data_manager.render_question_or_answer(data, question, question_id)
-            return render_template(html_file, question=question, answers=answers, q_head=q_head, a_head=a_head)
+            data_manager.render_question_or_answer(data, question, question_id)
+            return redirect('question/{}'.format(question_id))
         except:
             data = request.form.to_dict()
-            question, answers = data_manager.render_question_or_answer(data, question, question_id)
-            return render_template(html_file, question=question, answers=answers, q_head=q_head, a_head=a_head)
+            data_manager.render_question_or_answer(data, question, question_id)
+            return redirect('question/{}'.format(question_id))
 
     return render_template(html_file, question=question, answers=answers, q_head=q_head, a_head=a_head)
 
@@ -91,15 +94,14 @@ def update_question(question_id):
 @app.route('/question/<int:question_id>/vote-<dir>', methods=["POST"])
 def voter(question_id, dir):
     operatorr = operator.__add__ if dir == "up" else operator.__sub__
-    html_file = "question_with_answers.html"
     question, answers = data_manager.get_question_byid(question_id)
     data = request.form.to_dict()
     if "question" in data.keys():
-        question = data_manager.vote(question_id, question, data, operatorr)
-        return render_template(html_file, question=question, answers=answers, q_head=q_head, a_head=a_head)
+        data_manager.vote(question_id, question, data, operatorr)
+        return redirect('question/{}'.format(question_id))
     elif "answer" in data.keys():
-        answers = data_manager.vote(question_id, answers, data, operatorr)
-        return render_template(html_file, question=question, answers=answers, q_head=q_head, a_head=a_head)
+        data_manager.vote(question_id, answers, data, operatorr)
+        return redirect('question/{}'.format(question_id))
 
 
 @app.template_filter('ctime')
